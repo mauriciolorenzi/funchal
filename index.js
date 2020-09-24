9c5ff53c7fbd73efa59a490406f574ec096deec9
Mauricio Luis De Lorenzi <mauricio.lorenzi@toyota.com>
2020-09-23 22:20:57 -0300

    fixed some bugs and created a new page to manage the files.

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
-const { hostname } = require('os');
-
 const express = require('express'),
-app = express(),
-port = process.env.PORT || 4000;
+  app = express(),
+  port = process.env.PORT || 4000,
+  fs = require('fs'),
+  formidable = require('formidable');
 
-app.use('/almoco.pdf', express.static(`${__dirname}/almoco.pdf`));
-app.use('/pizzaria.pdf', express.static(`${__dirname}/pizzaria.pdf`));
-app.use('/render-pdf-almoco.js', express.static(`${__dirname}/render-pdf-almoco.js`));
-app.use('/render-pdf-pizzaria.js', express.static(`${__dirname}/render-pdf-pizzaria.js`));
+/* app.use('/files/almoco.pdf', express.static(`${__dirname}/files/almoco.pdf`));
+app.use('/files/pizzaria.pdf', express.static(`${__dirname}/files/pizzaria.pdf`)); */
+app.use('/js', express.static('js'));
+app.use('/css', express.static('css'));
+app.use('/files', express.static('files'));
+
+//app.use('/render-pdf.js', express.static(`${__dirname}/render-pdf.js`));
 
 app.get('/almoco', function (req, res) {
-    res.sendFile(`${__dirname}/almoco.html`);
-  });
+  res.sendFile(`${__dirname}/html/almoco.html`);
+});
 
-  app.get('/pizzaria', function (req, res) {
-    res.sendFile(`${__dirname}/pizzaria.html`);
+app.get('/pizzaria', function (req, res) {
+  res.sendFile(`${__dirname}/html/pizzaria.html`);
+
+});
+
+app.get('/alterpizzadocuments', function (req, res) {
+  res.sendFile(`${__dirname}/html/manage-files.html`);
+});
+
+app.post('/sendfiles', function (req, res) {
+  var form = formidable({ multiples: true });
+  form.parse(req, function (err, fields, files) {
+
+    files = files.files;
+
+    if (err) {
+      next(err);
+      return;
+    }
+
+    for (var i = 0; i < files.length; i++) {
+      let oldpath = files[i].path, newpath = `${__dirname}/files/${files[i].name}`;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) {
+          throw err;
+        }
+      });
+    }
+    res.redirect('/alterpizzadocuments');
+    res.end();
   });
+});
 
 app.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`)
-  });
+  console.log(`app listening at http://localhost:${port}`)
+});
